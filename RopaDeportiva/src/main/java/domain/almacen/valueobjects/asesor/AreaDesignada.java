cb36068ff9c0d05dde877ba34acdbf7a50df4ad8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 18:35:54 -0500

    se implementa el areaDesignada y HorasDeTrabajo

--- RopaDeportiva/src/main/java/domain/almacen/valueobjects/asesor/AreaDesignada.java
+++ RopaDeportiva/src/main/java/domain/almacen/valueobjects/asesor/AreaDesignada.java
@@ -1,4 +1,43 @@
 package domain.almacen.valueobjects.asesor;
 
-public class AreaDesignada {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class AreaDesignada implements ValueObject <AreaDesignada.Value> {
+
+    private final Value value;
+
+    // constructor
+    public AreaDesignada(Value value){
+        this.value = value;
+    }
+
+    // crear el enum
+    public enum Value{
+        VESTIDORES,
+        CAJA,
+        LOBBY,
+        ESTANTERIAS
+    }
+
+    // implementacion del metodo value
+    @Override
+    public Value value(){
+        return  value;
+    }
+
+    // se implementa el equal y hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AreaDesignada)) return false;
+        AreaDesignada that = (AreaDesignada) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
 }
