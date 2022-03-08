a6fdac416c3a7d276936ac85e50b4a49cf179c47
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 18:51:21 -0500

    se implementa la clase Asesor

--- RopaDeportiva/src/main/java/domain/almacen/entity/Asesor.java
+++ RopaDeportiva/src/main/java/domain/almacen/entity/Asesor.java
@@ -2,8 +2,54 @@ package domain.almacen.entity;
 
 import co.com.sofka.domain.generic.Entity;
 import domain.almacen.ids.AsesorId;
+import domain.almacen.valueobjects.asesor.AreaDesignada;
+import domain.almacen.valueobjects.asesor.HorasDeTrabajo;
+import domain.almacen.valueobjects.asesor.Identificacion;
+import domain.almacen.valueobjects.asesor.Nombre;
+
+import java.util.Objects;
 
 public class Asesor extends Entity<AsesorId> {
 
+    // se crean variables para los objectValue
 
+    private Nombre nombre;
+    private Identificacion identificacion;
+    private HorasDeTrabajo horasDeTrabajo;
+    private AreaDesignada areaDesignada;
+
+
+    // se crea un constructor publico para los objectValue
+
+    public Asesor(AsesorId asesorId, Nombre nombre, Identificacion identificacion, HorasDeTrabajo horasDeTrabajo, AreaDesignada areaDesignada){
+
+        // Super --> herencia
+        super(asesorId);
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.areaDesignada = areaDesignada;
+        this.horasDeTrabajo = horasDeTrabajo;
+    }
+
+    // métodos de la entidad Asesor
+
+    // cambiarNombre()
+    public void cambiarNombre(Nombre nombre){
+        this.nombre = Objects.requireNonNull(nombre);
+    }
+
+    // cambiarIdentificacion()
+    public void cambiarIdentificacion(Identificacion identificacion){
+        this.identificacion = Objects.requireNonNull(identificacion);
+    }
+
+    // cambbiarHorasDeTrabajo()
+    public void cambiarHorasDeTrabajo(HorasDeTrabajo horasDeTrabajo){
+        this.horasDeTrabajo = Objects.requireNonNull(horasDeTrabajo);
+    }
+
+    // cambiarAreaDesignada()
+    public void cambiarAreaDesignada(AreaDesignada areaDesignada){
+        this.areaDesignada = Objects.requireNonNull(areaDesignada);
+    }
 }
