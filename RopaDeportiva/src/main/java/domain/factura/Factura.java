c0ade33da892fc9072c0b4c7b0836a00ca4cee18
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-09 00:58:15 -0500

    se crea agregado factura

--- RopaDeportiva/src/main/java/domain/factura/Factura.java
+++ RopaDeportiva/src/main/java/domain/factura/Factura.java
@@ -1,4 +1,26 @@
 package domain.factura;
 
-public class Factura {
+import co.com.sofka.domain.generic.AggregateEvent;
+import domain.almacen.ids.AlmacenId;
+import domain.factura.entity.Cliente;
+import domain.factura.entity.Estado;
+import domain.factura.entity.Transaccion;
+import domain.factura.ids.FacturaId;
+
+public class Factura extends AggregateEvent<FacturaId> {
+
+    // se llaman las entidades de factura
+    // modificador de acceso
+    protected FacturaId facturaId;
+    protected Cliente cliente;
+    protected Transaccion transaccion;
+    protected Estado estado;
+    protected AlmacenId almacenId;
+
+    // se crea el constructor
+
+    public Factura(FacturaId entityId, AlmacenId almacenId) {
+        super(entityId);
+        this.almacenId = almacenId;
+    }
 }
