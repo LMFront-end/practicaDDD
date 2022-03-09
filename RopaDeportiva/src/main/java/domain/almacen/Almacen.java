c2a1b1ab70fd4f15d45a5debfbf99792c4d816f6
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 20:23:44 -0500

    se crea la funcion from para el almacen

--- RopaDeportiva/src/main/java/domain/almacen/Almacen.java
+++ RopaDeportiva/src/main/java/domain/almacen/Almacen.java
@@ -2,12 +2,14 @@ package domain.almacen;
 
 import co.com.sofka.domain.generic.AggregateEvent;
 import co.com.sofka.domain.generic.AggregateRoot;
+import co.com.sofka.domain.generic.DomainEvent;
 import domain.almacen.entity.Asesor;
 import domain.almacen.entity.Gerente;
 import domain.almacen.entity.Producto;
 import domain.almacen.event.AlmacenCreado;
 import domain.almacen.ids.AlmacenId;
 
+import java.util.List;
 import java.util.Set;
 
 
@@ -15,6 +17,7 @@ public class Almacen extends AggregateEvent<AlmacenId> {
 
     // se llaman las entidades de almacen
     // modificador de acceso
+    protected AlmacenId almacenId;
     protected Set <Asesor> asesor;
     protected Set <Producto> producto;
     protected Gerente gerente;
@@ -24,5 +27,18 @@ public class Almacen extends AggregateEvent<AlmacenId> {
     public Almacen(AlmacenId almacenId){
         super(almacenId);
         appendChange(new AlmacenCreado(almacenId)).apply();
+
+        subscribe(new AlmacenChange(this));
+    }
+
+    // se crea la funcion from para el almacen
+    // Metodo que permite reconstruir el estado del agregado a partir de una lista de eventos de dominios
+    public static Almacen from(AlmacenId almacenId, List<DomainEvent> events){
+
+        var almacen = new Almacen(almacenId);
+        // Evento de dominio, aplica el evento de dominio sobre el almacen
+        //events.forEach(event -> almacen.applyEvent(event))
+        events.forEach(almacen::applyEvent);
+        return almacen;
     }
 }
--- RopaDeportiva/src/main/java/domain/almacen/AlmacenChange.java
+++ RopaDeportiva/src/main/java/domain/almacen/AlmacenChange.java
@@ -1,4 +1,15 @@
 package domain.almacen;
 
-public class AlmacenChange {
+import co.com.sofka.domain.generic.EventChange;
+import domain.almacen.event.AlmacenCreado;
+
+
+public class AlmacenChange extends EventChange {
+
+    public AlmacenChange(Almacen almacen){
+
+        apply((AlmacenCreado event) -> {
+            almacen.almacenId = event.getAlmacenId();
+        });
+    }
 }
