8c7c0fd71168400e0ec2603bc68d3115d8a1d2f7
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-09 03:23:00 -0500

    se hacen los ultimos cambios

--- RopaDeportiva/src/test/java/domain/almacen/AlmacenTest.java
+++ RopaDeportiva/src/test/java/domain/almacen/AlmacenTest.java
@@ -3,13 +3,18 @@ package domain.almacen;
 import domain.almacen.ids.AlmacenId;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.matchers.JUnitMatchers;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class AlmacenTest {
 
     @Test
-    public void onCreateTest(){
+    void almacenTest(){
         var almacenId = AlmacenId.of("asdert");
         Almacen almacen = new Almacen(almacenId);
 
