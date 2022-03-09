1ed9d36c0c4354eccf274796e37f4b0812a472bf
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-09 00:18:41 -0500

    metodos entidad estado

--- RopaDeportiva/src/main/java/domain/factura/entity/Transaccion.java
+++ RopaDeportiva/src/main/java/domain/factura/entity/Transaccion.java
@@ -1,4 +1,16 @@
 package domain.factura.entity;
 
-public class Transaccion {
+import co.com.sofka.domain.generic.Entity;
+import domain.almacen.valueobjects.producto.Precio;
+import domain.factura.ids.TransaccionId;
+import domain.factura.valueobjects.Fecha;
+
+public class Transaccion extends Entity<TransaccionId> {
+
+    // se crean variables para los objectValue
+
+    private Fecha fecha;
+//    private Precio precio;
+
+
 }
