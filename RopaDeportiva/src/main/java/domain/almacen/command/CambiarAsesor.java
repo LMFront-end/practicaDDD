182d93b4570a5be4058f4b37f1fa3c86706d4f87
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 22:55:24 -0500

    se crean el comando para CambiarAsesor

--- RopaDeportiva/src/main/java/domain/almacen/command/CambiarAsesor.java
+++ RopaDeportiva/src/main/java/domain/almacen/command/CambiarAsesor.java
@@ -1,4 +1,47 @@
 package domain.almacen.command;
 
-public class CambiarAsesor {
+import co.com.sofka.domain.generic.Command;
+import domain.almacen.ids.AsesorId;
+import domain.almacen.valueobjects.asesor.AreaDesignada;
+import domain.almacen.valueobjects.asesor.HorasDeTrabajo;
+import generics.Identificacion;
+import generics.Nombre;
+
+public class CambiarAsesor extends Command {
+
+    private final AsesorId asesorId;
+    private final Nombre nombre;
+    private final Identificacion identificacion;
+    private final HorasDeTrabajo horasDeTrabajo;
+    private final AreaDesignada areaDesignada;
+
+    public CambiarAsesor(AsesorId asesorId, Nombre nombre, Identificacion identificacion, HorasDeTrabajo horasDeTrabajo, AreaDesignada areaDesignada){
+        this.asesorId = asesorId;
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.horasDeTrabajo = horasDeTrabajo;
+        this.areaDesignada = areaDesignada;
+    }
+
+    // se generan los getters
+
+    public AsesorId getAsesorId() {
+        return asesorId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Identificacion getIdentificacion() {
+        return identificacion;
+    }
+
+    public HorasDeTrabajo getHorasDeTrabajo() {
+        return horasDeTrabajo;
+    }
+
+    public AreaDesignada getAreaDesignada() {
+        return areaDesignada;
+    }
 }
