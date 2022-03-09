02788db9f87366e3b551bed9f8779b961c72a680
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-09 00:11:08 -0500

    creacion vo fecha

--- RopaDeportiva/src/main/java/domain/factura/entity/Estado.java
+++ RopaDeportiva/src/main/java/domain/factura/entity/Estado.java
@@ -1,4 +1,11 @@
 package domain.factura.entity;
 
-public class Estado {
+import co.com.sofka.domain.generic.Entity;
+import domain.factura.ids.EstadoId;
+import domain.factura.valueobjects.estado.EstadoActual;
+
+public class Estado extends Entity<EstadoId> {
+
+    private EstadoActual estadoActual;
+//    private Fecha fecha;
 }
