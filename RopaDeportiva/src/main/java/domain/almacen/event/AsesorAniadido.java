21a42e1f9bfd069f4803b8c303a54082f18a490f
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 22:21:41 -0500

    se crea el evento de AsesorAniadido

--- RopaDeportiva/src/main/java/domain/almacen/event/AsesorAniadido.java
+++ RopaDeportiva/src/main/java/domain/almacen/event/AsesorAniadido.java
@@ -1,4 +1,50 @@
 package domain.almacen.event;
 
-public class AsesorAniadido {
+import co.com.sofka.domain.generic.DomainEvent;
+import domain.almacen.ids.AsesorId;
+import domain.almacen.ids.ProductoId;
+import domain.almacen.valueobjects.asesor.AreaDesignada;
+import domain.almacen.valueobjects.asesor.HorasDeTrabajo;
+import domain.almacen.valueobjects.producto.*;
+import generics.Identificacion;
+import generics.Nombre;
+
+public class AsesorAniadido extends DomainEvent {
+
+    private final AsesorId asesorId;
+    private final Nombre nombre;
+    private final Identificacion identificacion;
+    private final HorasDeTrabajo horasDeTrabajo;
+    private final AreaDesignada areaDesignada;
+
+    public AsesorAniadido(AsesorId asesorId, Nombre nombre, Identificacion identificacion, HorasDeTrabajo horasDeTrabajo, AreaDesignada areaDesignada){
+        super("RopaDeportiva.ProductoAniadido");
+        this.asesorId = asesorId;
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.horasDeTrabajo = horasDeTrabajo;
+        this.areaDesignada = areaDesignada;
+    }
+
+    // se generan los getters
+
+    public AsesorId getAsesorId() {
+        return asesorId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Identificacion getIdentificacion() {
+        return identificacion;
+    }
+
+    public HorasDeTrabajo getHorasDeTrabajo() {
+        return horasDeTrabajo;
+    }
+
+    public AreaDesignada getAreaDesignada() {
+        return areaDesignada;
+    }
 }
