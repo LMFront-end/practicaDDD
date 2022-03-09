e898f918efd50ddd5784d52ee91e8ccc86ccce4a
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 22:52:31 -0500

    se crean el comando para AñadirGerente

--- RopaDeportiva/src/main/java/domain/almacen/command/AniadirGerente.java
+++ RopaDeportiva/src/main/java/domain/almacen/command/AniadirGerente.java
@@ -1,4 +1,33 @@
 package domain.almacen.command;
 
-public class AniadirGerente {
+import co.com.sofka.domain.generic.Command;
+import domain.almacen.ids.GerenteId;
+import generics.Identificacion;
+import generics.Nombre;
+
+public class AniadirGerente extends Command {
+
+    private final GerenteId gerenteId;
+    private final Nombre nombre;
+    private final Identificacion identificacion;
+
+    public AniadirGerente(GerenteId gerenteId, Nombre nombre, Identificacion identificacion){
+        this.gerenteId = gerenteId;
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+
+    // se generan los getters
+
+    public GerenteId getGerenteId() {
+        return gerenteId;
+    }
+
+    public Nombre getNombre() {
+        return nombre;
+    }
+
+    public Identificacion getIdentificacion() {
+        return identificacion;
+    }
 }
