4429a7c79df9d2e32bf4df4647e08e7370f9f4e5
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 23:06:08 -0500

    se crean el comando para EliminarProducto

--- RopaDeportiva/src/main/java/domain/almacen/command/EliminarProducto.java
+++ RopaDeportiva/src/main/java/domain/almacen/command/EliminarProducto.java
@@ -1,4 +1,20 @@
 package domain.almacen.command;
 
-public class EliminarProducto {
+import co.com.sofka.domain.generic.Command;
+import domain.almacen.ids.ProductoId;
+
+public class EliminarProducto extends Command {
+
+    private final ProductoId productoId;
+
+
+    public EliminarProducto(ProductoId productoId){
+        this.productoId = productoId;
+    }
+
+    // se genera el getter
+
+    public ProductoId getProductoId() {
+        return productoId;
+    }
 }
