ed9e1ce91804af0149d113c255b887b3e11a1da8
LMFront-end <perfilgitlinamaria@gmail.com>
2022-03-08 23:00:25 -0500

    se crean el comando para CambiarGerente

--- RopaDeportiva/src/main/java/domain/almacen/command/EliminarAsesor.java
+++ RopaDeportiva/src/main/java/domain/almacen/command/EliminarAsesor.java
@@ -1,4 +1,19 @@
 package domain.almacen.command;
 
-public class EliminarAsesor {
+import co.com.sofka.domain.generic.Command;
+import domain.almacen.ids.AsesorId;
+
+public class EliminarAsesor extends Command {
+
+    private final AsesorId asesorId;
+
+    public EliminarAsesor(AsesorId asesorId){
+        this.asesorId = asesorId;
+    }
+
+    // se genera getter
+    public AsesorId getAsesorId() {
+        return asesorId;
+    }
 }
+
